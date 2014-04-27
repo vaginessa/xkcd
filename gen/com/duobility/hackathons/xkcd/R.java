86bb13ca928833f8904295e27aecdb1dd686c0a7
arose13 <anthony_rose12@hotmail.com>
2014-04-27 14:00:21 -0400

    Demo functionality

--- gen/com/duobility/hackathons/xkcd/R.java
+++ gen/com/duobility/hackathons/xkcd/R.java
@@ -1037,12 +1037,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f05003e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int comicImageView=0x7f05003d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1056,6 +1057,7 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageCard=0x7f05003c;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1188,6 +1190,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int please_wait=0x7f0a0010;
     }
     public static final class style {
         public static final int ActionBar_Solid_Xkcddark=0x7f0b0086;
--- src/com/duobility/hackathons/xkcd/views/MainView.java
+++ src/com/duobility/hackathons/xkcd/views/MainView.java
@@ -4,13 +4,18 @@ import com.duobility.hackathons.xkcd.R;
 import com.duobility.hackathons.xkcd.activities.BaseActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainView extends BaseActivity {
+	
+	private ImageView comicImage;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_view);
+		
+		aq.id(comicImage).image("http://imgs.xkcd.com/comics/old_files.png");
 	}
 
 }
