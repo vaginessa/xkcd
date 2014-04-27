508b5f618bb91a5a55729cc3303fd96afe822542
arose13 <anthony_rose12@hotmail.com>
2014-04-27 11:49:26 -0400

    xkcd Constants adaded

--- src/com/duobility/hackathons/xkcd/data/AppConstants.java
+++
@@ -1,5 +0,0 @@
-package com.duobility.hackathons.xkcd.data;
-
-public class AppConstants {
-
-}
---
+++ src/com/duobility/hackathons/xkcd/data/XKCDConstants.java
@@ -0,0 +1,7 @@
+package com.duobility.hackathons.xkcd.data;
+
+public class XKCDConstants {
+	
+	public static final String XKCD_MAIN_URL = "http://xkcd.com/";
+	
+}
