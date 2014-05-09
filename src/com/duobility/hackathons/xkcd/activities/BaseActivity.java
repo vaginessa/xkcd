553551bf4120567ee37cb9b3bca28f71365e4b2f
arose13 <anthony_rose12@hotmail.com>
2014-05-09 16:30:21 -0400

    animations and activity transitions

--- src/com/duobility/hackathons/xkcd/activities/BaseActivity.java
+++ src/com/duobility/hackathons/xkcd/activities/BaseActivity.java
@@ -1,12 +1,20 @@
 package com.duobility.hackathons.xkcd.activities;
 
 import com.androidquery.AQuery;
+
+import com.duobility.hackathons.xkcd.R;
 import com.duobility.hackathons.xkcd.data.Fonts;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 public abstract class BaseActivity extends ActionBarActivity {
+	
+	public static String CLASSTAG = BaseActivity.class.getSimpleName();
+	
+	protected static boolean wifiConnected = false;
+	protected static boolean mobileConnected = false;
 	
 	protected Fonts mFonts = new Fonts();
 	protected AQuery aq;
@@ -17,4 +25,12 @@ public abstract class BaseActivity extends ActionBarActivity {
 		aq = new AQuery(this);
 	}
 	
+	/* Intents */
+	protected void gotoView(Intent toViewIntent) {
+		startActivity(toViewIntent);
+	}
+	
+	protected void activityTransitionAnimation_fromRight() {
+		overridePendingTransition(R.anim.activity_enter_fromright, R.anim.activity_nomotion);
+	}
 }
